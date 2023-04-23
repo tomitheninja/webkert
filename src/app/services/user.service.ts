e9a0951b2408b61ad46302127005436b7749806d
tomitheninja <sudi.tamas@simonyi.bme.hu>
2023-04-23 14:38:09 +0200

    userservice yields FSUser

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Auth, User } from '@angular/fire/auth';
-import { Observable } from 'rxjs';
+import { Auth } from '@angular/fire/auth';
+import { Observable, defaultIfEmpty, firstValueFrom } from 'rxjs';
+import { FSUser } from '../model/user';
+import { Firestore, doc, docData } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private user$: Observable<User | null>;
+  private user$: Observable<FSUser | null>;
   getStream() {
     return this.user$;
   }
-  getUser() {
-    return this.afAuth.currentUser;
-  }
-  constructor(private afAuth: Auth) {
-    this.user$ = new Observable((observer) => {
-      this.afAuth.onAuthStateChanged((user) => observer.next(user));
+  constructor(private afAuth: Auth, private fs: Firestore) {
+    this.user$ = new Observable<FSUser | null>((observer) => {
+      this.afAuth.onAuthStateChanged(async (user) => {
+        if (user) {
+          const userDoc = doc(this.fs, 'users', user.uid);
+          const userData = await firstValueFrom(
+            docData(userDoc, { idField: 'id' }).pipe(defaultIfEmpty(null))
+          );
+          observer.next(userData as FSUser);
+        } else {
+          observer.next(null);
+        }
+      });
     });
   }
 }
