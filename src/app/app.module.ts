466ca7d197c4808a8c907c556b598d562413d7eb
tomitheninja <sudi.tamas@simonyi.bme.hu>
2023-04-23 03:44:35 +0200

    Add firebase and material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'recipe-managment',
+    appId: '1:602306307295:web:10a47ced8a8faa35ecf56a',
+    storageBucket: 'recipe-managment.appspot.com',
+    apiKey: 'AIzaSyDKyFQ3ObaUQzqOQXCGNQVijHrKrqgWZX4',
+    authDomain: 'recipe-managment.firebaseapp.com',
+    messagingSenderId: '602306307295',
+    measurementId: 'G-5JK5XF7HMR',
+  },
+  production: false,
+};
